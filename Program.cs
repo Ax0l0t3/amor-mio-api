60fb9d0843fd0e31024ee06767fb78e242c8e0af
David Leon <david_leon@ign.com>
2025-05-11 18:12:47 -0600

    Endopints creation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/data-menu", async () => {
+    var filePath = "mockData-Menu.json";
+    var jsonContent = await File.ReadAllTextAsync(filePath);
+    return Results.Content(jsonContent, "application/json");
+});
+
+app.MapGet("/data-empty", async () => {
+    var filePath = "mockData-Empty.json";
+    var jsonContent = await File.ReadAllTextAsync(filePath);
+    return Results.Content(jsonContent, "application/json");
+});
+
+app.MapGet("/data-test-cases", async () => {
+    var filePath = "mockData-TestCases.json";
+    var jsonContent = await File.ReadAllTextAsync(filePath);
+    return Results.Content(jsonContent, "application/json");
+});
 
 app.Run();
