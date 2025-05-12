1043663e055fec5efc9ee53a1a0bcfeeda880455
David Leon <david_leon@ign.com>
2025-05-11 18:28:34 -0600

    Git ignore created

--- obj/Debug/net8.0/amor_mio_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/amor_mio_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("amor_mio_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2326391d66226d603ecc062feae3738707d18d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fb9d0843fd0e31024ee06767fb78e242c8e0af")]
 [assembly: System.Reflection.AssemblyProductAttribute("amor_mio_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("amor_mio_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
